72eae207a48b1de6ad7173c76ce2105bc24ff319
Arinc Sener <senerar-nc@windowslive.com>
2023-09-02 18:28:52 +0300

    first commit done with the finished product

---
+++ lib/dice_roller_state.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+final randomizer = Random(); //did this for efficiency
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+  var activeDiceImage = 'assets/images/dice-6.png';
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed:
+          rollDice, //function passed as a pointer but there is also an other way by defining anonymous function.
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
